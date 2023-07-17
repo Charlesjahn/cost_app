db3f752df393a6b59370dd3146605ab6307857c9
Charles Franklin Jahn <charles.jahn@gmail.com>
2023-07-17 21:03:09 +0100

    new project class

--- src/components/pages/NewProjects.js
+++ src/components/pages/NewProjects.js
@@ -1,6 +1,13 @@
-function NewProject(){
-    return(
-        <h1>NewProject</h1>
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
+
+function NewProject() {
+    return (
+        <div className={styles.newprojeect_container}>
+            <h1>New Project</h1>
+            <p>Create a new project</p>
+            <ProjectForm btnText="Create a project"/>
+        </div>
     )
 }
 export default NewProject
