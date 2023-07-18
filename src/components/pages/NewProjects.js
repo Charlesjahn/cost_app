0b80220eda2cb4b9ae4ddb90191cb43609f0f3ed
Charles Franklin Jahn <charles.jahn@gmail.com>
2023-07-18 18:39:03 +0100

    Update New project.js

--- src/components/pages/NewProjects.js
+++ src/components/pages/NewProjects.js
@@ -1,12 +1,37 @@
-import styles from './NewProject.module.css'
+import { useNavigate } from 'react-router-dom'
+
+import styles from './NewProjects.module.css'
+
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost(project) {
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                history('/projects', { state: { message: "Project created" } })
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.newprojeect_container}>
             <h1>New Project</h1>
             <p>Create a new project</p>
-            <ProjectForm btnText="Create a project"/>
+            <ProjectForm handleSubmit={createPost} btnText="Create a project" />
         </div>
     )
 }
