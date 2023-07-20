d31f06d57c5837fe6a6353884119c1624563f84d
Charles Franklin Jahn <charles.jahn@gmail.com>
2023-07-20 18:39:09 +0100

    Create loading icon

---
+++ src/components/layout/Loading.js
@@ -0,0 +1,14 @@
+import styles from './Loading.module.css'
+
+import loading from '../../img/loading.svg'
+
+function Loading() {
+    return (
+
+        < div className={styles.loading_container} >
+            <img className={styles.load_img} src={loading} alt="Loading" />
+        </div >
+    )
+}
+
+export default Loading
