0c1cee7b6d82d0d16973320a8d96c796e9866eeb
Charles Franklin Jahn <charles.jahn@gmail.com>
2023-07-20 18:39:24 +0100

    Update Project

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from "../project/ProjectCard"
 
@@ -10,6 +11,8 @@ import styles from './Projects.module.css'
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [removeLoad, setRemoveLoad] = useState(false)
+    const [projectMsg, setProjectMsg] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -31,9 +34,26 @@ function Projects() {
         }).then(resp => resp.json())
             .then((data) => {
                 setProjects(data)
+                setRemoveLoad(true)
             })
             .catch((err) => console.log(err))
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        })
+            .then(resp => resp.json)
+            .then(data => {
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMsg('Project Deleted')
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -44,16 +64,22 @@ function Projects() {
 
             {message && <Message type="success" msg={message} />}
 
+            {projectMsg && <Message type="delete" msg={projectMsg} />}
+
             <Container customClass="start">
                 {projects.length > 0 &&
-                    projects.map((project) =>
+                    projects.map((project) => (
                         <ProjectCard
                             name={project.name}
                             id={project.id}
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
-                        />)}
+                            handleRemove={removeProject}
+                        />
+                    ))}
+                {!removeLoad && <Loading />}
+                {removeLoad && projects.length === 0 && ( <Message type="empty" msg={"0 project saved"}/>)}
             </Container>
 
         </div>
